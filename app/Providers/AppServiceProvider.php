80b08226c52ed24aa9a9751ad1e45954da44669f
kylen-dev <justineroien@gmail.com>
2025-09-22 12:25:33 +0800

    AppServiceProvider and Trust Proxies

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // Force HTTPS in production (important on Render)
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         // Automatically create storage link if it doesn't exist
         $link = public_path('storage');
         $target = storage_path('app/public');
